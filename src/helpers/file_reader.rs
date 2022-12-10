7064a02895d95a26fc20043ec3d261e50057f63a
David Pate <2223120+DavidTPate@users.noreply.github.com>
2022-12-10 09:02:54 -0700

    Added day1 working code with module

--- src/helpers/file_reader.rs
+++ src/helpers/file_reader.rs
@@ -1,10 +1,28 @@
 use std::{env, fs};
+use std::error::Error;
 use std::path::PathBuf;
+use std::str::Split;
+
+const USE_TEST_INPUT: bool = false;
 
 fn get_current_working_dir() -> PathBuf {
     return env::current_dir().unwrap();
 }
 
-pub fn read_file_in_cwd(file: &str) -> String {
-    return fs::read_to_string(get_current_working_dir().join(file)).unwrap();
+pub fn read_file_in_cwd(path: String) -> String {
+    match fs::read_to_string(get_current_working_dir().join(path.clone())) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file at path {} with {:?}", path, error),
+    }
+}
+
+pub fn read_file_for_day(day: &str) -> String {
+    return read_file_in_cwd(if USE_TEST_INPUT { format!("../input/day{}-test.txt", day) } else { format!("../input/day{}.txt", day) });
+}
+
+pub fn get_block_endings(input: &str) -> &str {
+    return if input.contains("\r") { "\r\n\r" } else { "\n\n" };
+}
+pub fn get_line_endings(input: &str) -> &str {
+    return if input.contains("\r") { "\r\n" } else { "\n" };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
+use std::borrow::Borrow;
+
 mod helpers;
 
 fn main() {
-    let useTestInput = false;
-    let input = helpers::file_reader::read_file_in_cwd(if useTestInput { "../input/day1-test.txt" } else { "../input/day1.txt" });
-    let windowsEndings = input.contains("\r");
-    let blockSeparator = if windowsEndings { "\r\n\r" } else { "\n\n" };
-    let lineSeparator = if windowsEndings { "\r\n" } else { "\n" };
-    let elves: Vec<&str> = input.split(blockSeparator).map(|s| s.trim()).filter(|s| !s.is_empty()).collect();
-    let elvesDetailInv: Vec<Vec<i32>> = elves.iter().map(|inv| inv.split(lineSeparator).map(|x| x.parse().unwrap()).collect()).collect();
-    let mut elvesTotalInv: Vec<i32> = elvesDetailInv.iter().map(|x| x.into_iter().sum()).collect();
-    elvesTotalInv.sort_by(|a, b| b.cmp(a));
+    day2();
+}
 
-    if (useTestInput) {
-        assert_eq!(24000, elvesTotalInv[0]);
-        assert_eq!(11000, elvesTotalInv[1]);
-        assert_eq!(10000, elvesTotalInv[2]);
-        assert_eq!(6000, elvesTotalInv[3]);
-        assert_eq!(4000, elvesTotalInv[4]);
-    } else {
-        println!("First Answer {:?}", elvesTotalInv[0]);
-        println!("Second Answer {:?}", elvesTotalInv[0] + elvesTotalInv[1] + elvesTotalInv[2]);
+fn day2() {
 
-    }
+}
+
+fn day1() {
+    let input = helpers::file_reader::read_file_for_day("1");
+    let block_endings = helpers::file_reader::get_block_endings(input.borrow());
+    let line_endings = helpers::file_reader::get_line_endings(input.borrow());
+    let mut elf_inv: Vec<u32> = input.split(block_endings).map(|s| {
+        let inv = s.trim().split(line_endings);
+        let mut accum = 0;
+        for item in inv {
+            accum += item.parse::<u32>().unwrap();
+        }
+        return accum;
+    }).collect();
+    elf_inv.sort_by(|a, b| b.cmp(a));
+    println!("First Answer {:?}", elf_inv[0]);
+    println!("Second Answer {:?}", elf_inv[0] + elf_inv[1] + elf_inv[2]);
 }
